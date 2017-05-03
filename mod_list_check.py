665a10d8df8bf1a56dfacf1812ef86f68c9f5226
Noah Kiss <contact@noahkiss.com>
2017-05-03 19:52:19 -0400

    just updating some stuff.

--- mod_list_check.py
+++ mod_list_check.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
 
-'''
-check users that mod AR
-  grab perms, require mail perms
-verify list against modmail sub approved submitters (optional?)
-add as approved submitters
-'''
+import praw
 
+from secrets import client_id, client_secret, password, user_agent, username, main_sub, modmail_sub, backroom_sub
+try:
+  from secrets import unban_sub
+except ImportError:
+  unban_sub = False
+
+r = praw.Reddit(client_id=client_id,
+                client_secret=client_secret,
+                user_agent=user_agent,
+                username=username,
+                password=password)
+
+if unban_sub != False:
+  unban_sub  = r.subreddit(unban_sub)
+main_sub     = r.subreddit(main_sub)
+modmail_sub  = r.subreddit(modmail_sub)
+backroom_sub = r.subreddit(backroom_sub)
+
+if __name__ == '__main__':
+  moderators = []
+  for mod in main_sub.moderator():
+    if 'all' in mod.mod_permissions or 'mail' in mod.mod_permissions:
+      try:
+        modmail_sub.contributor.add(mod.name)
+      except:
+        pass
+      try: 
+        unban_sub.contributor.add(mod.name)
+      except:
+        pass
+      try: 
+        backroom_sub.contributor.add(mod.name)
+      except:
+        pass
--- modmail_actions.py
+++ modmail_actions.py
@@ -4,5 +4,6 @@
 read each mod's actions over the month
 add up, sort by actions in array
 make post to backroom with month's actions
+reset db at end of month? save copy?
 '''
 
