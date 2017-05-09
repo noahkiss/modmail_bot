c7c9e5f09ef34564fb1dbfb6f0af0755373a5156
Noah Kiss <contact@noahkiss.com>
2017-05-09 03:55:13 -0400

    working hard or hardly working?

--- modmail_archive.py
+++ modmail_archive.py
@@ -13,7 +13,10 @@ read modmail
 
 check for artwork for unbans - sent by user, image link
 private moderator note to remind of unban image text format?
+private mod note for suicide thingy?
 '''
+
+from time import sleep
 
 import sqlite3
 import praw
@@ -23,12 +26,6 @@ try:
   from secrets import unban_sub
 except ImportError:
   unban_sub = False
-
-modmail_db   = sqlite3.connect(modmail_db)
-mods_db      = sqlite3.connect(mods_db)
-
-modmail_curs = modmail_db.cursor()
-mods_curs    = mods_db.cursor()
 
 r = praw.Reddit(client_id=client_id,
                 client_secret=client_secret,
@@ -43,30 +40,74 @@ sub_modmail  = r.subreddit(modmail_sub)
 sub_backroom = r.subreddit(backroom_sub)
 
 def read_modmail():
-  pass
+  for mail in sub_main.modmail.conversations():
+    
+    post_body = ""
 
-def mailNew(id):
-  pass
+    title = mail.authors[0].name + " | " + mail.subject + " (" + mail.messages[0].date[:10] + ")"
 
-def mailOld(id):
-  pass
+    for message in mail.messages:
+      post_body += "####/u/" + message.author.name + ":"
+      if message.is_internal:
+        post_body += " (Private)"
+      if mail.authors[0].name == message.author.name:
+        post_body += " (OP)"
+      post_body +=  "\n\n" + message.body_markdown + "\n\n---\n\n"
 
-def mailMod(id):
-  pass
+    mail_exists, num_replies = db_read(id=mail.id)
 
-def threadPost(user, title, flair=None):
-  pass
+    if mail_exists:
+      if mail.num_messages > num_replies:
+        mail_old(mail.id, post_body)
+        #count all new mails, add mod action for mod
+    else:
+      #if mail.is_internal: figure out how to read mod discussions tbd
+      # if mail.subject == "You've been banned from participating in r/" + mail.owner.display_name:
+      #   title += " [Ban thread]" just do this with automod?
+      mail_new(mail.id, post_body, title)
+      #add mod actions for mods
 
-def threadUpdate(id):
-  pass
+def mail_new(id, body, title):
+  print(title)
+  #write db for new mail here after submitting
 
-def postArt(user, title, url):
-  if unban_sub == False:
-    pass
-  else:
-    pass
+def mail_old(id, body):
+  print(id)
+
+# def post_art(user, title, image_url):
+#   if unban_sub != False:
+#     post_title = title + " by /u/" + user
+#     sub_unban.submit(post_title, url=image_url, send_replies=False)
+#   else:
+#     pass
+
+def db_read(id):
+  exists = False
+  db = sqlite3.connect(modmail_db)
+  curs = db.cursor()
+
+  #exists = True
+  #read post id, if it exists return true and actions, if not return false and zero
+
+  db.commit()
+  db.close()
+  return exists, 2
+
+def db_write(id):
+  db = sqlite3.connect(modmail_db)
+  curs = db.cursor()
+
+  db.commit()
+  db.close()
+
+def db_mod_numbers(user):
+  db = sqlite3.connect(mods_db)
+  curs = db.cursor()
+
+  db.commit()
+  db.close()
 
 if __name__ == '__main__':
-  pass
-#modmailDB.commit()
-#modmailDB.close()
+  while True:
+    read_modmail()
+    sleep(30)
