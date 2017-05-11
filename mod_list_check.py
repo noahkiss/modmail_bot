b2fff6c1694da901a7a4cbdf5c46a1d416b43717
Noah Kiss <contact@noahkiss.com>
2017-05-11 00:52:04 -0400

    less api hits means faster scripts yo

--- mod_list_check.py
+++ mod_list_check.py
@@ -2,11 +2,7 @@
 
 import praw
 
-from secrets import client_id, client_secret, password, user_agent, username, main_sub, modmail_sub, backroom_sub
-try:
-  from secrets import unban_sub
-except ImportError:
-  unban_sub = False
+from secrets import client_id, client_secret, password, user_agent, username, main_sub, modmail_sub
 
 r = praw.Reddit(client_id=client_id,
                 client_secret=client_secret,
@@ -14,25 +10,29 @@ r = praw.Reddit(client_id=client_id,
                 username=username,
                 password=password)
 
-if unban_sub != False:
-  sub_unban  = r.subreddit(unban_sub)
 sub_main     = r.subreddit(main_sub)
 sub_modmail  = r.subreddit(modmail_sub)
-sub_backroom = r.subreddit(backroom_sub)
 
 if __name__ == '__main__':
   moderators = []
-  for mod in sub_main.moderator():
+  contributors = []
+
+  mods = sub_main.moderator()
+  approved = sub_modmail.contributor()
+
+  for mod in mods:
     if 'all' in mod.mod_permissions or 'mail' in mod.mod_permissions:
-      try:
-        sub_modmail.contributor.add(mod.name)
-      except:
-        pass
-      try: 
-        sub_unban.contributor.add(mod.name)
-      except:
-        pass
-      try: 
-        sub_backroom.contributor.add(mod.name)
-      except:
-        pass
+      moderators.append(mod.name)
+
+  for contributor in approved:
+    contributors.append(contributor.name)
+
+  for user in moderators:
+    if user not in contributors:
+      sub_modmail.contributor.add(user)
+      print("Adding " + user + " to " + modmail_sub)
+
+  for user in contributors:
+    if user not in moderators:
+      sub_modmail.contributor.remove(user)
+      print("Removing " + user + " from " + modmail_sub)
