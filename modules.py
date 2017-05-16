a486cac515fec1474d6f8c595182486eee11825b
Noah Kiss <contact@noahkiss.com>
2017-05-16 19:49:30 -0400

    added modlog to modules

--- modules.py
+++ modules.py
@@ -1,3 +1,4 @@
 # delete comment chain
 # etc
 # reply to message if ban in title and not you've been banned, add private note to link to original ban message if found
+# check moderator log to find actions done
